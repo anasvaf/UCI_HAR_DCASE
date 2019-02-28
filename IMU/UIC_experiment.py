7a3d211f63db8b983696f253f6dbc0f727ba086b
fcruciani <federicocruciani@gmail.com>
2019-02-28 11:56:51 +0000

    CNN-Feature extractor training

--- IMU/UIC_experiment.py
+++ IMU/UIC_experiment.py
@@ -7,8 +7,8 @@ classes = ["WALKING", "WALK_UPSTAIRS", "WALK_DOWNSTAIRS", "SITTING", "STANDING",
 
 
 def train_CNN_feature_extractor():
-	X_train, labels_train, list_ch_train = read_data(data_path=datapath, split="train") # train
-	X_test, labels_test, list_ch_test = read_data(data_path=datapath, split="test") # test
+	X_train, labels_train, list_ch_train = UCI_HAR.read_data(data_path=datapath, split="train") # train
+	X_test, labels_test, list_ch_test = UCI_HAR.read_data(data_path=datapath, split="test") # test
 	assert list_ch_train == list_ch_test, "Mistmatch in channels!"
 	X_train, X_test = standardize(X_train, X_test)
 	print("Data size:", len(X_train), " - ", len(X_train[0]))
@@ -31,13 +31,14 @@ def train_CNN_feature_extractor():
 	predictions = clf.predict(X_test,batch_size=1)
 	predictions_inv = [ [np.argmax(x)] for x in predictions]
 	
-	clf.printClassificationReport(true=all_test,pred=all_preds,classes=classes,filename="CNN_LSTM_original_report.txt")
-	clf.plotConfusionMatrix(true=all_test,pred=all_preds,classes=classes,showGraph=True,saveFig=True,filename="CNN_LSTM_original_CM.png")
+	clf.printClassificationReport(true=y_test,pred=predictions_inv,classes=classes,filename="CNN_LSTM_original_report.txt")
+	clf.plotConfusionMatrix(true=y_test,pred=predictions_inv,classes=classes,showGraph=True,saveFig=True,filename="CNN_LSTM_original_CM.png")
 	  
 	  
 
 def mainMenu():
 	print("1. Train CNN feature extractor 1 \n2. Selection 2\n\n Press any other key to exit")
+	sel = input("")
 
 
 
